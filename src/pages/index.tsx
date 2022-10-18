2c465ff42118fe4c83736393f0b3bdfd9194aa02
Rino Tovar <rinotova@gmail.com>
2022-10-18 13:52:36 +0200

    Push

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,19 @@
-import type { NextPage } from "next";
+import type { NextPage } from 'next';
+import { trpc } from '../utils/trpc';
 
 const Home: NextPage = () => {
+  const { data, isLoading } = trpc.example.hello.useQuery({
+    text: 'from tRPC',
+  });
 
   return (
-    <div className="h-screen w-screen flex flex-col justify-center items-center">
-      <div className="text-2xl text-center">Which Pokemon is Rounder</div>
-      <div className="p-2"/>
-      <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
-        <div className="w-16 h-16 bg-red-200"/>
-         <div className="p-8">Vs</div>
-         <div className="w-16 h-16 bg-red-200"/>
+    <div className="flex h-screen w-screen flex-col items-center justify-center">
+      <div className="text-center text-2xl">Which Pokemon is Rounder</div>
+      <div className="p-2" />
+      <div className="flex max-w-2xl items-center justify-between rounded border p-8">
+        <div className="h-16 w-16 bg-red-200" />
+        <div className="p-8">Vs</div>
+        <div className="h-16 w-16 bg-red-200" />
       </div>
     </div>
   );
